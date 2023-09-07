e361e9d7b36e0c60ce837902aef21cb39757d1bc
Max Luan <max_luan@foxmail.com>
2023-09-07 23:44:15 +0800

    performance analysis, drawer header with image, no divider, round corner drawer

--- sleep-as-android-clone/index.md
+++ sleep-as-android-clone/index.md
@@ -8,3 +8,8 @@ I don't really know how to optimize this. I can think of one of the following re
 
 The second approach seems to need a few seconds to warm up while the first doesn't. 
 I will look into this later.
+
+## Conclusion
+It turns out, if I actually built the app in apk and installed in on my phone, isntead of using debug mode, the animation would be very smotth. 
+
+The lession is, don't optimize ahead of time. Or if you want to, at least test it in a production environment. 
--- sleep-as-android-clone/search-focus-with-result-dynamic-overlay-explicit-animation.dart
+++ sleep-as-android-clone/search-focus-with-result-dynamic-overlay-explicit-animation.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() {
-  debugRepaintRainbowEnabled = true;
+  debugRepaintRainbowEnabled = false;
   runApp(const MyApp());
 }
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
         title: "My App",
         home: HomePage()
     );
@@ -19,6 +20,8 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -88,7 +91,9 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
   }
 
   void _openDrawer() {
-    print("tapped");
+    if (kDebugMode) {
+      print("tapped");
+    }
     _scaffoldKey.currentState?.openDrawer();
   }
 
@@ -107,134 +112,138 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     return Scaffold(
       key: _scaffoldKey,
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(80.0),
-        child: AnimatedBuilder(
-          animation: controller,
-          builder: (context, child){
-            return SafeArea(
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Focus(
-                        onFocusChange: (hasFocus) {
-                          if (hasFocus) {
-                            setState(() {
-                              _isSearchExpanded = true;
-                              controller.forward();
-                            });
-                          } else {
-                            setState(() {
-                              _isSearchExpanded = false;
-                              controller.reverse();
-                            });
-                          }
-                        },
-                        child: RepaintBoundary(
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(16.0 - 16*controller.value, 16 - 16*controller.value, 16 - 16*controller.value, 16),
-                            child: Container(
-                                height: 64+ 32 *controller.value,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8.0), // Adjust the corner radius as needed
-                                  color: Colors.grey[200], // Adjust the background color as needed
-                                ),
-                                child: Stack(
-                                  children: [
-                                    TextField(
-                                      focusNode: _searchFocusNode,
-                                      decoration: InputDecoration(
-                                        hintText: 'Search',
-                                        border: InputBorder.none,
-                                        contentPadding: EdgeInsets.fromLTRB(64.0, 12.0 + 8*controller.value, 12.0, 12.0), // Adjust padding as needed
-                                        suffixIcon: !_isSearchExpanded? IconButton(
-                                            icon: const Icon(Icons.clear),
-                                            onPressed: () {
-                                              // Clear th`e search text
-                                              _searchFocusNode.unfocus( );
-                                            }
+          preferredSize: const Size.fromHeight(80.0),
+          child: AnimatedBuilder(
+            animation: controller,
+            builder: (context, child){
+              return SafeArea(
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Focus(
+                          onFocusChange: (hasFocus) {
+                            if (hasFocus) {
+                              setState(() {
+                                _isSearchExpanded = true;
+                                controller.forward();
+                              });
+                            } else {
+                              setState(() {
+                                _isSearchExpanded = false;
+                                controller.reverse();
+                              });
+                            }
+                          },
+                          child: RepaintBoundary(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(16.0 - 16*controller.value, 16 - 16*controller.value, 16 - 16*controller.value, 16),
+                              child: Container(
+                                  height: 64+ 32 *controller.value,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8.0), // Adjust the corner radius as needed
+                                    color: Colors.grey[200], // Adjust the background color as needed
+                                  ),
+                                  child: Stack(
+                                    children: [
+                                      TextField(
+                                        focusNode: _searchFocusNode,
+                                        decoration: InputDecoration(
+                                          hintText: 'Search',
+                                          border: InputBorder.none,
+                                          contentPadding: EdgeInsets.fromLTRB(64.0, 12.0 + 8*controller.value, 12.0, 12.0), // Adjust padding as needed
+                                          suffixIcon: !_isSearchExpanded? IconButton(
+                                              icon: const Icon(Icons.clear),
+                                              onPressed: () {
+                                                // Clear th`e search text
+                                                _searchFocusNode.unfocus( );
+                                              }
 
-                                        ): null,
+                                          ): null,
+                                        ),
                                       ),
-                                    ),
 
-                                    Positioned(
-                                        top: controller.value * 8,
-                                        child:
-                                        !_isSearchExpanded?
-                                        TextButton(onPressed: _openDrawer, child: const Icon(Icons.menu)): TextButton(
-                                            onPressed: _unfocusInput, child: const Icon(Icons.arrow_back))
-                                    )
-                                  ],
-                                )
+                                      Positioned(
+                                          top: controller.value * 8,
+                                          child:
+                                          !_isSearchExpanded?
+                                          TextButton(onPressed: _openDrawer, child: const Icon(Icons.menu)): TextButton(
+                                              onPressed: _unfocusInput, child: const Icon(Icons.arrow_back))
+                                      )
+                                    ],
+                                  )
+                              ),
                             ),
                           ),
-                        ),
-                      )
-                  )
-                ],
-              ),
-            );
-          },
-        )
+                        )
+                    )
+                  ],
+                ),
+              );
+            },
+          )
       ),
       body: Center(
         child: Stack(
             children: [
               if(!_isSearchExpanded) screens[_selectedIndex],
               RepaintBoundary(
-                child: AnimatedOpacity(duration: const Duration(milliseconds: 300),
-                  opacity: _isSearchExpanded? 1: 0,
-                  child: ListView.builder(
-                    itemCount: 10, // Example number of results
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text('Result $index'),
-                        // Add your result item widget here
-                      );
-                    },
-                  ),
-                )
+                  child: AnimatedOpacity(duration: const Duration(milliseconds: 300),
+                    opacity: _isSearchExpanded? 1: 0,
+                    child: ListView.builder(
+                      itemCount: 10, // Example number of results
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text('Result $index'),
+                          // Add your result item widget here
+                        );
+                      },
+                    ),
+                  )
               )
             ]
         ),
       ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('First'),
-              selected: _selectedIndex == 0,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(0);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Second'),
-              selected: _selectedIndex == 1,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(1);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            )
-          ],
+      drawer: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topRight: Radius.circular(30),
+          bottomRight: Radius.circular(30)
         ),
-      ),
+        child: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(64, 64, 64, 32),
+                  child: Image.asset("assets/images/cloudy_sun.png", fit: BoxFit.fitHeight)
+              ),
+              ListTile(
+                title: const Text('First'),
+                selected: _selectedIndex == 0,
+                onTap: () {
+                  // Update the state of the app
+                  _onItemTapped(0);
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Second'),
+                selected: _selectedIndex == 1,
+                onTap: () {
+                  // Update the state of the app
+                  _onItemTapped(1);
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
