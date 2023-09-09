d4fa1f79c7f10b31892726aeb593682da3b1c059
Max Luan <max_luan@foxmail.com>
2023-09-09 16:33:12 +0800

    color scheme, icon drawer, reduced space

--- sleep-as-android-clone/search-focus-with-result-dynamic-overlay-explicit-animation.dart
+++ sleep-as-android-clone/search-focus-with-result-dynamic-overlay-explicit-animation.dart
@@ -12,9 +12,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        title: "My App",
-        home: HomePage()
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme(
+          primary: Colors.orange,
+          primaryContainer: Colors.orange[700],
+          secondary: Colors.green,
+          secondaryContainer: Colors.green[700],
+          brightness: Brightness.light,
+          onPrimary: Colors.white,
+          onSecondary: Colors.black,
+          onSurface: Colors.black,
+          onError: Colors.white,
+            error: Colors.red,
+            background: Colors.grey[200]!,
+          surface: Colors.white, onBackground: Colors.black
+        )
+      ),
+      title: "My App",
+      home: const HomePage()
     );
   }
 }
@@ -110,140 +126,145 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     screens.add(const SecondScreen());
 
     return Scaffold(
-      key: _scaffoldKey,
-      appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(80.0),
-          child: AnimatedBuilder(
-            animation: controller,
-            builder: (context, child){
-              return SafeArea(
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: Focus(
-                          onFocusChange: (hasFocus) {
-                            if (hasFocus) {
-                              setState(() {
-                                _isSearchExpanded = true;
-                                controller.forward();
-                              });
-                            } else {
-                              setState(() {
-                                _isSearchExpanded = false;
-                                controller.reverse();
-                              });
-                            }
-                          },
-                          child: RepaintBoundary(
-                            child: Padding(
-                              padding: EdgeInsets.fromLTRB(16.0 - 16*controller.value, 16 - 16*controller.value, 16 - 16*controller.value, 16),
-                              child: Container(
-                                  height: 64+ 32 *controller.value,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8.0), // Adjust the corner radius as needed
-                                    color: Colors.grey[200], // Adjust the background color as needed
-                                  ),
-                                  child: Stack(
-                                    children: [
-                                      TextField(
-                                        focusNode: _searchFocusNode,
-                                        decoration: InputDecoration(
-                                          hintText: 'Search',
-                                          border: InputBorder.none,
-                                          contentPadding: EdgeInsets.fromLTRB(64.0, 12.0 + 8*controller.value, 12.0, 12.0), // Adjust padding as needed
-                                          suffixIcon: !_isSearchExpanded? IconButton(
-                                              icon: const Icon(Icons.clear),
-                                              onPressed: () {
-                                                // Clear th`e search text
-                                                _searchFocusNode.unfocus( );
-                                              }
+        key: _scaffoldKey,
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(80.0),
+            child: AnimatedBuilder(
+              animation: controller,
+              builder: (context, child){
+                return SafeArea(
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Focus(
+                            onFocusChange: (hasFocus) {
+                              if (hasFocus) {
+                                setState(() {
+                                  _isSearchExpanded = true;
+                                  controller.forward();
+                                });
+                              } else {
+                                setState(() {
+                                  _isSearchExpanded = false;
+                                  controller.reverse();
+                                });
+                              }
+                            },
+                            child: RepaintBoundary(
+                              child: Padding(
+                                padding: EdgeInsets.fromLTRB(16.0 - 16*controller.value, 16 - 16*controller.value, 16 - 16*controller.value, 16),
+                                child: Container(
+                                    height: 64+ 32 *controller.value,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8.0), // Adjust the corner radius as needed
+                                      color: Colors.grey[200], // Adjust the background color as needed
+                                    ),
+                                    child: Stack(
+                                      children: [
+                                        TextField(
+                                          focusNode: _searchFocusNode,
+                                          decoration: InputDecoration(
+                                            hintText: 'Search',
+                                            border: InputBorder.none,
+                                            contentPadding: EdgeInsets.fromLTRB(64.0, 12.0 + 8*controller.value, 12.0, 12.0), // Adjust padding as needed
+                                            suffixIcon: !_isSearchExpanded? IconButton(
+                                                icon: const Icon(Icons.clear),
+                                                onPressed: () {
+                                                  // Clear th`e search text
+                                                  _searchFocusNode.unfocus( );
+                                                }
 
-                                          ): null,
+                                            ): null,
+                                          ),
                                         ),
-                                      ),
 
-                                      Positioned(
-                                          top: controller.value * 8,
-                                          child:
-                                          !_isSearchExpanded?
-                                          TextButton(onPressed: _openDrawer, child: const Icon(Icons.menu)): TextButton(
-                                              onPressed: _unfocusInput, child: const Icon(Icons.arrow_back))
-                                      )
-                                    ],
-                                  )
+                                        Positioned(
+                                            top: controller.value * 8,
+                                            child:
+                                            !_isSearchExpanded?
+                                            TextButton(onPressed: _openDrawer, child: const Icon(Icons.menu)): TextButton(
+                                                onPressed: _unfocusInput, child: const Icon(Icons.arrow_back))
+                                        )
+                                      ],
+                                    )
+                                ),
                               ),
                             ),
-                          ),
-                        )
+                          )
+                      )
+                    ],
+                  ),
+                );
+              },
+            )
+        ),
+        body: Center(
+          child: Stack(
+              children: [
+                if(!_isSearchExpanded) screens[_selectedIndex],
+                RepaintBoundary(
+                    child: AnimatedOpacity(duration: const Duration(milliseconds: 300),
+                      opacity: _isSearchExpanded? 1: 0,
+                      child: ListView.builder(
+                        itemCount: 10, // Example number of results
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text('Result $index'),
+                            // Add your result item widget here
+                          );
+                        },
+                      ),
                     )
-                  ],
-                ),
-              );
-            },
-          )
-      ),
-      body: Center(
-        child: Stack(
-            children: [
-              if(!_isSearchExpanded) screens[_selectedIndex],
-              RepaintBoundary(
-                  child: AnimatedOpacity(duration: const Duration(milliseconds: 300),
-                    opacity: _isSearchExpanded? 1: 0,
-                    child: ListView.builder(
-                      itemCount: 10, // Example number of results
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text('Result $index'),
-                          // Add your result item widget here
-                        );
-                      },
-                    ),
-                  )
-              )
-            ]
-        ),
-      ),
-      drawer: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          topRight: Radius.circular(30),
-          bottomRight: Radius.circular(30)
-        ),
-        child: Drawer(
-          // Add a ListView to the drawer. This ensures the user can scroll
-          // through the options in the drawer if there isn't enough vertical
-          // space to fit everything.
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: [
-              Padding(
-                  padding: const EdgeInsets.fromLTRB(64, 64, 64, 32),
-                  child: Image.asset("assets/images/cloudy_sun.png", fit: BoxFit.fitHeight)
-              ),
-              ListTile(
-                title: const Text('First'),
-                selected: _selectedIndex == 0,
-                onTap: () {
-                  // Update the state of the app
-                  _onItemTapped(0);
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: const Text('Second'),
-                selected: _selectedIndex == 1,
-                onTap: () {
-                  // Update the state of the app
-                  _onItemTapped(1);
-                  // Then close the drawer
-                  Navigator.pop(context);
-                },
-              )
-            ],
+                )
+              ]
           ),
         ),
-      )
+        drawer: ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(30),
+              bottomRight: Radius.circular(30)
+          ),
+          child: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              children: [
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(64, 64, 64, 32),
+                    child: Image.asset("assets/images/cloudy_sun.png", fit: BoxFit.fitHeight)
+                ),
+                ListTile(
+                  contentPadding: const EdgeInsets.only(left: 40),
+                  minLeadingWidth: 10,
+                  leading: const Icon(Icons.home),
+                  title: const Text('First'),
+                  selected: _selectedIndex == 0,
+                  onTap: () {
+                    // Update the state of the app
+                    _onItemTapped(0);
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  contentPadding: const EdgeInsets.only(left: 40),
+                  minLeadingWidth: 10,
+                  leading: const Icon(Icons.account_circle),
+                  title: const Text('Second'),
+                  selected: _selectedIndex == 1,
+                  onTap: () {
+                    // Update the state of the app
+                    _onItemTapped(1);
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+          ),
+        )
     );
   }
 }
